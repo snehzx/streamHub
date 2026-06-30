f1b23a305bce37a587eb1735d9e06462448138b6
snehzx <24CSEAIML075@jssaten.ac.in>
2026-06-30 18:44:12 +0530

    aggregate pipelines to fetch watch history & channel info

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logOutUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.midleware.js";
@@ -28,5 +35,16 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJwt, logOutUser);
 router.route("/refreshToken").post(refreshAccessToken);
+router.route("/changePassword").post(verifyJwt, changeCurrentPassword);
+router.route("/getCurrentUser").get(verifyJwt, getCurrentUser);
+router.route("/updateAccountDetails").patch(verifyJwt, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/coverImage")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+router.route("/channel/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/watchHistory").get(verifyJwt, getWatchHistory);
 
 export default router;
